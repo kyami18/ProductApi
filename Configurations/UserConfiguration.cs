f5af732bfbd86a3850d8da942d6968112b694b85
kyami <nguyenlenhatminh2003@gmail.com>
2026-09-10 16:09:45 +0700

    Configure User entity

---
+++ Configurations/UserConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProductApi.Models;
+
+namespace ProductApi.Configurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.HasKey(u => u.Id);
+
+        builder.Property(u => u.Username)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.HasIndex(u => u.Username)
+            .IsUnique();
+
+        builder.Property(u => u.Password)
+            .IsRequired();
+
+        builder.Property(u => u.Role)
+            .IsRequired()
+            .HasMaxLength(20);
+
+        builder.Property(u => u.RefreshToken)
+            .HasMaxLength(200);
+    }
+}
