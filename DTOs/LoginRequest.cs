58c39421d70f5fbe89f3879049bfb1b475f876ae
kyami <nguyenlenhatminh2003@gmail.com>
2026-09-10 16:39:08 +0700

    Add login request validation

--- DTOs/LoginRequest.cs
+++ DTOs/LoginRequest.cs
@@ -1,12 +1,16 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿
+
+using System.ComponentModel.DataAnnotations;
 
 namespace ProductApi.DTOs;
 
 public class LoginRequest
 {
     [Required]
+    [MaxLength(50)]
     public string Username { get; set; } = string.Empty;
 
     [Required]
+    [MinLength(6)]
     public string Password { get; set; } = string.Empty;
 }
